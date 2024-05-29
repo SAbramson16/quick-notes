340e8499ac59e0ec6794d0e8b04b0be65079085b
Sharon Abramson <sharabramson@gmail.com>
2024-05-29 16:44:57 -0400

    debug deployment

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import 'semantic-ui-css/semantic.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './App';
+import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
